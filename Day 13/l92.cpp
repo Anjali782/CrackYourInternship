052fcacbfddabe81556206be4e2e828153cba3bf
Anjali782 <77624000+Anjali782@users.noreply.github.com>
2022-06-06 22:59:10 +0530

    Create l92.cpp

---
+++ Day 13/l92.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+  ListNode* reverseBetween(ListNode* head, int left, int right) {
+   vector v;
+   int j=0;
+   ListNode* temp=head;
+      
+   while(temp!=NULL){
+   v.push_back(temp->val);
+   temp=temp->next;
+   }
+   reverse(v.begin()+left-1,v.begin()+right);
+   
+       ListNode* temp1=head;
+       while(temp1!=NULL){
+           temp1->val=v[j];
+           j++;
+           temp1=temp1->next; 
+       }
+     return head;
+  }
+};
