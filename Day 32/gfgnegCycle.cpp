f211d6ad60f055147f0ba2c38462f6fbb0436e55
Anjali Gupta <77624000+Anjali782@users.noreply.github.com>
2022-06-25 11:06:31 +0530

    Create gfgnegCycle.cpp

---
+++ Day 32/gfgnegCycle.cpp
@@ -0,0 +1,29 @@
+bool isNegCycleBellmanFord(struct Graph* graph,
+                           int src)
+{
+    int V = graph->V;
+    int E = graph->E;
+    int dist[V];
+    for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX;
+    dist[src] = 0;
+    for (int i = 1; i <= V - 1; i++) {
+        for (int j = 0; j < E; j++) {
+            int u = graph->edge[j].src;
+            int v = graph->edge[j].dest;
+            int weight = graph->edge[j].weight;
+            if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
+                dist[v] = dist[u] + weight;
+        }
+    }
+  
+    for (int i = 0; i < E; i++) {
+        int u = graph->edge[i].src;
+        int v = graph->edge[i].dest;
+        int weight = graph->edge[i].weight;
+        if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
+            return true;
+    }
+  
+    return false;
+}
